f542cfa5cbe2378689ec0afa82df794a859d91c7
Jose de Assis <jassis@cgctec.com.br>
2018-10-10 06:37:11 -0300

    Alterando o template de email

--- index.js
+++ index.js
@@ -2,7 +2,57 @@ const server = require('./config/server');
 const knex = require('./config/dbConnection');
 const consign = require('consign');
 const errs = require('restify-errors');
+const nodemailer = require('nodemailer');
+const handlebars = require("node-handlebars");
 
+
+server.post('/email', (req, res) => {
+
+    const $usuario = 'contato@cgctec.com.br';
+    const $senha = 'Jssajj99-';
+
+    const hbs = handlebars.create({
+        partialsDir: __dirname
+    });
+
+    hbs.engine(__dirname + "\\views\\resetPasswordCGCTec.html", {name: "Jakob"}, function (err, html) {
+        if (err) {
+            throw err;
+        }
+        const $html = "'" + html + "'";
+
+        const transporter = nodemailer.createTransport({
+            host: 'mail.cgctec.com.br',
+            port: 465,
+            secure: true, // use TLS
+            auth: {
+                user: $usuario,
+                pass: $senha
+            },
+            tls: {
+                // do not fail on invalid certs
+                rejectUnauthorized: false
+            }
+        });
+
+
+        const $destinatario = 'jassis@ibist.org.br';
+
+        const mailOptions = {
+            from: 'CGC Tecnologia <' + $usuario + '>',
+            to: $destinatario,
+            subject: 'CGC Tecnologia - Reset de Senha',
+            html: $html
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            return !error ? res.send('Email enviado: ' + info.response) : res.send('Erro no envio do Email: ' + error);
+        });
+
+    });
+
+
+});
 
 
 // rotas REST para o user_type
@@ -17,3 +67,4 @@ consign().include('./restfull/userstatus').into(server, knex, errs);
 
 consign().include('./restfull/user').into(server, knex, errs);
 
+// Eviando um e-mail
